acef6a3dbac37cf3547199d492068f2942e0c38f
nikhilsaxena07 <nikhil.saxena@joshtechnology.com>
2025-07-17 19:53:31 +0530

    NS_R1_00: Updated Folder Structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,3 @@
 import { Routes } from '@angular/router';
-import { NotFoundComponent } from '@modules/not-found/not-found.component';
-import { LoginComponent } from '@pages/auth/login/login.component';
-import { SignupComponent } from '@pages/auth/signup/signup.component';
-import { DashboardComponent } from '@pages/dashboard/dashboard.component';
 import { HomeComponent } from '@pages/home/home.component';
-import { SearchPageComponent } from '@pages/search-page/search-page.component';
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'admin', component: DashboardComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'search', component: SearchPageComponent },
-  { path: '**', component: NotFoundComponent },
-];
+export const routes: Routes = [{ path: '', component: HomeComponent }];
